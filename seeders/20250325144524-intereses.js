ff7171df7939bf8dc630f43873413361b66f70b5
natalie <ntrodriguezch@hotmail.com>
2025-03-25 09:53:58 -0500

    2. crear seeder para intereses

---
+++ seeders/20250325144524-intereses.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const intereses = ["Tecnología",  "Ciberseguridad",  "Desarrollo de Software",  "Inteligencia Artificial",  "Ciencia de Datos",  "Blockchain",  "Internet de las Cosas (IoT)",  "Marketing Digital",  "Videojuegos",  "Finanzas y Criptomonedas"];
+    for (let interes of intereses){
+      console.log(interes);
+      await
+      queryInterface.bulkInsert('intereses', [{
+        descripcion: interes,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('intereses', null, {});
+  }
+};
